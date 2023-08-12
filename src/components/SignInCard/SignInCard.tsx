28d46e1751903faa4ecad1d643f75a31c95d9609
kamalesh.suresh <kamalesh.suresh@psiog.com>
2023-08-12 22:17:33 +0530

    Added Sign in page

--- src/components/SignInCard/SignInCard.tsx
+++ src/components/SignInCard/SignInCard.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, TextField, Button } from '@mui/material'
 import React from 'react'
 import './SignInCard.css'
 
@@ -8,6 +8,22 @@ function SignIncard (): JSX.Element {
       <Typography className='heading1'>
       Sign in
       </Typography>
+    </Grid>
+    <Grid item container xs={12} direction="column" justifyContent="space-evenly"alignItems="stretch" height="70%">
+      <TextField
+          className='textbox'
+          id="standard-password-input"
+          label="User name"
+          variant="standard"
+        />
+        <TextField
+          className='textbox'
+          id="standard-password-input"
+          label="Password"
+          variant="standard"
+          type='password'
+        />
+        <Button variant="contained">Sign in</Button>
     </Grid>
   </Grid>
 }
